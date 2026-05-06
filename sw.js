68a2b781f79a32f13358d4f8ff4c8c7b23e58c70
Formedil Padova - Area Sicurezza e Salute <cptpd@did.scuolaedilepadova.net>
2026-05-06 06:50:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
 // Service Worker – Stradario di Padova
-const CACHE_NAME = 'stradario-pd-v2';
+const CACHE_NAME = 'stradario-pd-v4';
 const ASSETS = [
   './index.html',
   './manifest.json',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/apple-touch-icon.png',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-384x384.png',
   'https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;600;700;900&family=Barlow+Condensed:wght@700;900&display=swap'
 ];
 
